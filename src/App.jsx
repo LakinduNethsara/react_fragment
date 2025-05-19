234a5af7117625039b5c4603572e5969a92b16bb
Lakindu Nethsara <lakindunethsara378@gmail.com>
2025-05-20 00:37:05 +0530

    Initialize

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+  const nameList = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Heidi', 'Ivan', 'Judy','Kathy', 'Leo', 'Mallory', 'Nina', 'Oscar', 'Peggy', 'Quentin', 'Rupert', 'Sybil', 'Trent','Uma','Victor','Walter','Xena','Yara','Zane']
+  
+
+  
+}
+
+export default App
